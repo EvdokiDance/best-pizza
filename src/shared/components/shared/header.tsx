c426edcd642819a3b87366cd154a738c9c28f47b
Alexander Evdokimov <evdoki15@yandex.ru>
2024-08-11 23:25:39 +0300

    refact and feats

--- src/shared/components/shared/header.tsx
+++ src/shared/components/shared/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container, SearchInput } from '.';
 import Image from 'next/image';
 import { Button } from '..';
-import {  ShoppingCart, User } from 'lucide-react';
+import {  ArrowRight, ShoppingCart, User } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/shared/lib';
 
@@ -25,14 +25,24 @@ export const Header: React.FC<Props> = ({ className }) => {
             <div className='mx-10 flex-1'>
                 <SearchInput/>       
             </div>
+           <div className='flex gap-3'>
             <div className='flex gap-3'>
-                <Button variant='outline' className='flex items-center gap-1'>
-                    <User size={16}/><div>Войти</div>
-                </Button>
-                <Button variant='outline'>
-                    <ShoppingCart size={16} />
-                </Button>
-            </div>
+                    <Button variant='outline' className='flex items-center gap-1'>
+                        <User size={16}/><div>Войти</div>
+                    </Button>
+                </div>
+                <div>
+                    <Button className='group relative'>
+                        <b>520 ₽</b>
+                        <span className='h-full w-[1px] bg-white/30 mx-3'/>
+                        <div className='flex items-center gap-1  transition-all duration-300 group-hover:opacity-0'>
+                            <ShoppingCart size={16} />
+                            <b>3</b>
+                        </div>
+                        <ArrowRight size={20} className='absolute  transition-all duration-300 -translate-x-2 group-hover:opacity-100 right-5 group-hover:right-3 opacity-0'/>
+                    </Button>
+                </div>
+           </div>
         </Container>
     </header>
   );
